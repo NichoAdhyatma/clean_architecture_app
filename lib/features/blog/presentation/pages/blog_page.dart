bc31724486e78ed348bbaf2994f1285fa9855a0d
NichoAdhyatma <nicholausadhyatma@gmail.com>
2024-07-09 15:15:39 +0700

    card ui

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -1,5 +1,9 @@
+import 'package:blog_app_clean_tdd/core/helper/sizedbox_ext.dart';
+import 'package:blog_app_clean_tdd/core/theme/app_pallete.dart';
+import 'package:blog_app_clean_tdd/core/utils/show_snackbar.dart';
 import 'package:blog_app_clean_tdd/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:blog_app_clean_tdd/features/blog/presentation/pages/add_new_blog_page.dart';
+import 'package:blog_app_clean_tdd/features/blog/presentation/widgets/blog_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -44,10 +48,33 @@ class _BlogPageState extends State<BlogPage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          "User is signed in",
-        ),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            showSnackbar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is BlogDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+
+                return BlogCard(
+                  blog: blog,
+                  color: AppPallete.gradient1,
+                );
+              },
+            );
+          }
+
+          return 0.heightSb;
+        },
       ),
     );
   }
---
+++ lib/features/blog/presentation/widgets/blog_card.dart
@@ -0,0 +1,62 @@
+import 'package:blog_app_clean_tdd/features/blog/domain/entities/blog.dart';
+import 'package:flutter/material.dart';
+
+class BlogCard extends StatelessWidget {
+  final Blog blog;
+  final Color color;
+
+  const BlogCard({
+    super.key,
+    required this.blog,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 10,
+      ),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: blog.topics
+                  .map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.only(
+                        right: 5,
+                      ),
+                      child: Chip(
+                        label: Text(e),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              blog.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            contentPadding: EdgeInsets.zero,
+            titleTextStyle: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const Text("1 min")
+        ],
+      ),
+    );
+  }
+}
